19fe632af88931b7aedf493f2b9d0fede8d41573
rasdani <73563550+rasdani@users.noreply.github.com>
2025-03-29 17:07:05 +0000

    config as in simpleRL zoo

--- simple_train.py
+++ simple_train.py
@@ -1,8 +1,12 @@
+import os
+
 from trl import GRPOTrainer, GRPOConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 
 from reward import compute_score
+
+os.environ["WANDB_PROJECT"] = "smolGRPO"
 
 def reward_func(completions, ground_truth, **kwargs):
     rewards = []
@@ -11,6 +15,9 @@ def reward_func(completions, ground_truth, **kwargs):
         reward = compute_score(content, gt)
         rewards.append(reward["score"])
     return rewards
+
+def calc_gradient_accumulation_steps(num_gpus, per_device_train_batch_size, target_batch_size=1024):
+    return int(target_batch_size / (per_device_train_batch_size * num_gpus))
 
 model_name = "Qwen/Qwen2.5-0.5B"
 model = AutoModelForCausalLM.from_pretrained(model_name)
@@ -22,35 +29,37 @@ dataset = load_dataset("hkust-nlp/SimpleRL-Zoo-Data",
 train_dataset = dataset.rename_column(original_column_name="gt_answer", new_column_name="ground_truth")
 
 num_gpus = 2
+# per_device_train_batch_size = 4
+per_device_train_batch_size = 8
 run_name = "simple_rl_zoo_" + model_name.split("/")[-1].lower()
+
 
 training_args = GRPOConfig(
     output_dir=f"outputs/{run_name}",
     run_name=run_name,
-    learning_rate=1e-6,
-    lr_scheduler_type="constant_with_warmup",
-    warmup_steps=20,
+    learning_rate=5e-7,
+    learning_rate_scheduler="constant",
     num_train_epochs=1,
     bf16=True,
-    adam_beta1=0.9,
-    adam_beta2=0.99,
-    max_grad_norm=0.1,
     num_iterations=1,
-    # beta=0.04,
-    beta=0.001,
+    beta=0.0001,
+    epsilon=0.2,
     max_prompt_length=1024,
-    max_completion_length=1024,
-    per_device_train_batch_size=4,
+    max_completion_length=8192,
+    temperature=1.0,
+    # per_device_train_batch_size=4,
     # per_device_train_batch_size=32,
     # per_device_train_batch_size=16,
-    num_generations=(2 * num_gpus - 2 if num_gpus > 1 else 2),
-    # num_generations=8,
-    gradient_accumulation_steps=int(16 / num_gpus),
+    per_device_train_batch_size=per_device_train_batch_size,
+    # num_generations=(2 * num_gpus - 2 if num_gpus > 1 else 2),
+    num_generations=8,
+    # gradient_accumulation_steps=int(16 / num_gpus),
+    gradient_accumulation_steps=calc_gradient_accumulation_steps(num_gpus, per_device_train_batch_size),
     # gradient_accumulation_steps=2,
     gradient_checkpointing=True,
     save_strategy="steps",
-    # save_steps=100,
-    save_steps=200,
+    save_steps=100,
+    # save_steps=200,
     save_only_model=True,
     use_vllm=True,
     vllm_device=f"cuda:{num_gpus-1}",
@@ -60,11 +69,13 @@ training_args = GRPOConfig(
     log_on_each_node=True,
     log_completions=True,
     report_to="wandb",
+    apply_chat_template=False,
     )
 
 trainer = GRPOTrainer(
     model=model,
     args=training_args,
+    # tokenizer=tokenizer,
     reward_funcs=reward_func,
     train_dataset=train_dataset,
     # eval_dataset=eval_dataset,
